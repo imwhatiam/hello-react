f3a07f81b5be3f2466869985e37c8d34b011d1d8
lian <lian@seafile.com>
2021-08-16 17:09:46 +0800

    component tree

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 自定义的组件都必须要用大写字母开头，普通的 HTML 标签都用小写字母开头。
+class Title extends Component {
+  render () {
+    return (
+      <h1>This is Title</h1>
+    )
+  }
+}
+
+class Header extends Component {
+  render () {
+    return (
+    <div>
+      <Title />
+      <h2>This is Header</h2>
+    </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+    <div>
+      <h2>This is main content</h2>
+    </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render () {
+    return (
+    <div>
+      <h2>This is footer</h2>
+    </div>
+    )
+  }
+}
+
+// 理解组件树的概念对后面理解数据是如何在组件树内自上往下流动过程很重要。
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
